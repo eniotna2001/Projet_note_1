dd868f2955a0eeb5ef50a9e4e8758c0eafa319a2
eniotna92 <2001lelongantoine@gmail.com>
2020-10-14 12:01:33 +0200

    first commit

--- TPNoté1.java
+++ TPNoté1.java
@@ -20,20 +20,22 @@ class Personne{
     }
 
 public class Architecte extends Personne{
-    yeyeyeyeyeyey
-    ……………
+    private String conseilRegional;
+    private boolean inscriptionOrdre;
     public Architecte(String nom, String prenom, String conseilRegional, boolean
-    inscriptionOrdre, Adresse adresse, String telephone)
-    {
-super(nom, prenom);
-this.conseilRegional = ………………………………………………………………………………………..;
-this.inscriptionOrdre = inscriptionOrdre;
-this.adresse = adresse;
-if (telephoneValide(telephone))
-{ this.telephone = telephone; }
-else
-{ this.telephone = ""; }
-Maj_conseilRegional();
+    inscriptionOrdre, Adresse adresse, String telephone){
+        super(nom, prenom);
+        this.conseilRegional = ………………………………………………………………………………………..;
+        this.inscriptionOrdre = inscriptionOrdre;
+        this.adresse = adresse;
+        if (telephoneValide(telephone)){
+            this.telephone = telephone;
+        } 
+    }
+    else{
+        this.telephone = "";
+    }
+    Maj_conseilRegional();
 }
 private boolean telephoneValide(String telephone){ ……………………………………………………}
 public void Maj_conseilRegional() { ………………………………………………………………….}
