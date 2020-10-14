bd8c490fa0137b75e189a0c9d6bfde56431a50c3
eniotna92 <2001lelongantoine@gmail.com>
2020-10-14 11:56:11 +0200

    first commit

--- TPNoté1.java
+++ TPNoté1.java
@@ -19,3 +19,31 @@ class Personne{
         public String toString() {return this.nom + " " + this.prenom; }
     }
 
+public class Architecte extends Personne{
+    ……………
+    ……………
+    public Architecte(String nom, String prenom, String conseilRegional, boolean
+    inscriptionOrdre, Adresse adresse, String telephone)
+    {
+super(nom, prenom);
+this.conseilRegional = ………………………………………………………………………………………..;
+this.inscriptionOrdre = inscriptionOrdre;
+this.adresse = adresse;
+if (telephoneValide(telephone))
+{ this.telephone = telephone; }
+else
+{ this.telephone = ""; }
+Maj_conseilRegional();
+}
+private boolean telephoneValide(String telephone){ ……………………………………………………}
+public void Maj_conseilRegional() { ………………………………………………………………….}
+// renvoie une chaine contenant le nom, le prenom, le conseil regional et
+// le numero de telephone de l'architecte ainsi que la mention "inscrit"
+// s'il est inscrit au conseil de l'ordre ou "non inscrit" dans le cas contraire
+public String toString () {
+String chaine = "";
+…………………………
+chaine = chaine + this.conseilRegional + " ";
+………………………………………………………………
+………………………………………………………………
+}
