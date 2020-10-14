f3c60af155258c49cae93ba1fe4ef27a6c325f64
eniotna92 <2001lelongantoine@gmail.com>
2020-10-14 12:15:07 +0200

    first commit

---
+++ TPNote1.java
@@ -0,0 +1,21 @@
+package TPNoté1;
+/**
+ *
+ * @author antoine
+ */
+
+class Personne{
+    private String nom;
+    private String prenom;
+    
+    public Personne(String nom , String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+    
+    private String validate (String chaine, Integer taille){ // Vérifie la taille et renvoie une chaîne tronquée
+        // si la taille est supérieure à taille
+        return chaine; }
+        public String toString() {return this.nom + " " + this.prenom; }
+    }
+
--- Traitement.java
+++ Traitement.java
@@ -1,4 +1,7 @@
 package tpnoté1;
+
+import java.lang.*;
+import java.util.*;
 /**
  *
  * @author antoine
@@ -9,7 +12,8 @@ public class Traitement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Personne perso1 = new Personne("Lelong","Antoine");
+        System.out.println(perso1.toString());
     }
     
 }
