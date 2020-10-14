72aa4c3117a813c125c8b9bd08970a69bad53b09
eniotna92 <2001lelongantoine@gmail.com>
2020-10-14 11:01:56 +0200

    first commit

---
+++ Traitement.java
@@ -0,0 +1,22 @@
+package tpnoté1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antoi
+ */
+public class Traitement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
