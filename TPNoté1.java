261f11e50f87e61bef40bee0889d15fe30dea9e6
eniotna92 <2001lelongantoine@gmail.com>
2020-10-14 11:47:50 +0200

    third commit

--- TPNoté1.java
+++ TPNoté1.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tpnoté1;
-
+package TPNoté1;
 /**
  *
- * @author antoi
+ * @author antoine
  */
-public class TPNoté1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class Personne{
+    private String nom;
+    private String prenom;
+    
+    public Personne(String nom , String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
     }
     
-}
+    private String validate (String chaine, Integer taille){ // Vérifie la taille et renvoie une chaîne tronquée
+        // si la taille est supérieure à taille
+        return chaine; }
+        public String toString() {return this.nom + " " + this.prenom; }
+    }
+
--- Traitement.java
+++ Traitement.java
@@ -1,14 +1,7 @@
 package tpnoté1;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author antoi
+ * @author antoine
  */
 public class Traitement {
 
